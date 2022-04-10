386dbc3d181b7d212d2034c6f81542da39fdd880
KUSHALjain3218 <74366948+KUSHALjain3218@users.noreply.github.com>
2022-04-10 17:14:31 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,17 @@
+class money:
+    def __init__(self,balance):
+        
+        self.balance = balance
+        
+    def change_balance(self,bal):
+        self.balance = bal
+        print(f"your account has a balnance of {self.balance}")
+  
+    
+
+Aarav =money(10000)
+
+
+
+
+            
